e34ee998d7dec2018797aea5c05b36ec707c9942
GitHub Action <action@github.com>
2022-07-22 05:08:21 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/muthuishere/darttestgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.muthuishere.darttestgenerator.services
+
+import com.github.muthuishere.darttestgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/muthuishere/darttestgenerator/MyPluginTest.kt
+++ src/test/kotlin/com/github/muthuishere/darttestgenerator/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.muthuishere.darttestgenerator
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
